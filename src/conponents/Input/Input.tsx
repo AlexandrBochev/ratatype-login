e0b91ef9f724fee1984c287d980af6bf7159a85c
Alexandr Bochev <abochev@gmail.com>
2023-11-29 12:57:26 +0200

    peer

--- src/conponents/Input/Input.tsx
+++ src/conponents/Input/Input.tsx
@@ -12,14 +12,26 @@ const Input = ({ type, label, handleChange, id, isValid }: InputProps) => {
       <input
         id={ id }
         className={`
-          w-full h-[3.75rem] sm:h-[4.375rem] border-[0.125rem] border-gray bg-ligth_gray rounded-[0.625rem] text-black py-[1.125rem] sm:py-[1.25rem] px-[0.9375rem]
-          focus:outline-none focus:border-brand focus:pb-0 valid:pb-0 ${!isValid && 'border-red'}
+          w-full h-[3.75rem] sm:h-[4.375rem] border-[0.125rem] border-gray bg-ligth_gray rounded-[0.625rem]
+          text-black py-[1.125rem] sm:py-[1.25rem] px-[0.9375rem]
+          focus:outline-none focus:border-brand focus:pb-0 valid:pb-0 peer ${!isValid && 'border-red'}
         `}
         type={ type }
         required
         onChange={ (e) => handleChange(e) }
       />
-      <label className="absolute top-[1.125rem] sm:top-[1.25rem] left-[0.937rem] transition-all pointer-events-none">{ label }</label>
+
+      <label
+        className="
+          absolute top-[1.125rem] sm:top-[1.25rem] left-[0.937rem]
+          peer-focus:top-[0.25rem] peer-focus:text-[0.875rem] sm:peer-focus:text-[1rem]
+          peer-valid:top-[0.25rem] peer-valid:text-[0.875rem] sm:peer-valid:text-[1rem]
+          pointer-events-none transition-all
+        "
+      >
+        { label }
+      </label>
+
       {id === 'email' && !isValid && <p className="text-[0.875rem] sm:text-[1rem] text-red">Вкажіть вашу ел. пошту</p>}
       {id === 'password' && !isValid && <p className="text-[0.875rem] sm:text-[1rem] text-red">Мінімум 8 символів: цифри, великі та малі літери (a-z).</p>}
       {id === 'password' &&
